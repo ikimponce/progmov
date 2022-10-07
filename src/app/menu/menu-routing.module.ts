f2361e6e5f61d9012fe8d8cd97b3207980c0a17a
unknown <ikimponce@gmail.com>
2022-10-07 16:41:49 -0300

    second comm

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -9,8 +9,8 @@ const routes: Routes = [
     component: MenuPage,
     children: [
       {
-        path: "home",
-        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule),
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
       },
       {
         path: 'about',
